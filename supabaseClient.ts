1925ff8de90f9ddce38e1185d3808e2111ff00e8
enghenriquecarvalho-lab <eng.henriquecarvalho@gmail.com>
2025-11-10 11:33:52 -0300

    Update supabaseClient.ts

--- supabaseClient.ts
+++ supabaseClient.ts
@@ -4,7 +4,7 @@ import { createClient } from '@supabase/supabase-js';
 // Cole suas próprias credenciais do Supabase abaixo.
 // Você pode encontrá-las no painel do seu projeto no Supabase, em Configurações > API.
 
-const supabaseUrl = 'COLE_SUA_URL_SUPABASE_AQUI';
-const supabaseAnonKey = 'COLE_SUA_CHAVE_ANON_SUPABASE_AQUI';
+export const supabaseUrl = 'https://vizmsoqabwwperqrpvff.supabase.co';
+export const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZpem1zb3FhYnd3cGVycXJwdmZmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI1MDE2NTQsImV4cCI6MjA3ODA3NzY1NH0.TO7-yCeLri74jKahHJwzakNJKDLT49RwmDrws8V4o5U';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
