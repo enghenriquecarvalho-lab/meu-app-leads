0183c90fbef3ef9965ffb5ab596cc897a5f9cbc4
enghenriquecarvalho-lab <eng.henriquecarvalho@gmail.com>
2025-11-10 11:54:12 -0300

    Update supabaseClient.ts

--- supabaseClient.ts
+++ supabaseClient.ts
@@ -4,9 +4,5 @@ import { createClient } from '@supabase/supabase-js';
 export const supabaseUrl = 'https://vizmsoqabwwperqrpvff.supabase.co';
 export const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZpem1zb3FhYnd3cGVycXJwdmZmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI1MDE2NTQsImV4cCI6MjA3ODA3NzY1NH0.TO7-yCeLri74jKahHJwzakNJKDLT49RwmDrws8V4o5U';
 
-// Se as linhas acima ainda contiverem os textos de exemplo, a aplicação não funcionará.
-if (supabaseUrl.includes('https://vizmsoqabwwperqrpvff.supabase.co') || supabaseAnonKey.includes('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZpem1zb3FhYnd3cGVycXJwdmZmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI1MDE2NTQsImV4cCI6MjA3ODA3NzY1NH0.TO7-yCeLri74jKahHJwzakNJKDLT49RwmDrws8V4o5U')) {
-  throw new Error("As credenciais do Supabase não foram configuradas. Por favor, edite o arquivo services/supabaseClient.ts no GitHub.");
-}
-
+// A verificação das chaves agora é feita no App.tsx para mostrar um erro amigável.
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
